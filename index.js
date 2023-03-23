e0796d51e1f58df0682f059a1bf0a803df62896b
lordish <gitonga403@gmail.com>
2023-03-23 09:17:39 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",function (){
+    console.log("Load after");
+});
+
+
+const text = document.getElementById("text");
+    console.log(document.body.replace = "This is really cool"); 
+ 
